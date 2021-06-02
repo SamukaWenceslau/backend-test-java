6ddef6a9f05a56ea4e760bd22643128190ba6525
Samuel.WL <samukawens@gmail.com>
2021-06-02 10:24:25 -0300

    Gerando estrutura base do projeto

---
+++ parking-project/src/test/java/br/com/fcamara/parkingproject/ParkingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcamara.parkingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
