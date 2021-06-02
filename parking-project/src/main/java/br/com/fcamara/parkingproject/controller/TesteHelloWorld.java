fec4d8b36b9065eb4baa120ea4770a92b3c04ce6
Samuel.WL <samukawens@gmail.com>
2021-06-02 10:41:03 -0300

    configurando banco de dados e testando aplicação

---
+++ parking-project/src/main/java/br/com/fcamara/parkingproject/controller/TesteHelloWorld.java
@@ -0,0 +1,18 @@
+package br.com.fcamara.parkingproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TesteHelloWorld {
+
+	
+	@GetMapping
+	@ResponseBody
+	public String welcome() {
+		return "Hello World!";
+	}
+}
