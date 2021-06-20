869643f01d7f6a30e1eb24980d35a7a9ddcf0e60
Samuel.WL <samukawens@gmail.com>
2021-06-20 15:27:04 -0300

    lidando com exception dinamicamente

--- parking-project/src/main/java/br/com/fcamara/parkingproject/service/ParkingManagerService.java
+++ parking-project/src/main/java/br/com/fcamara/parkingproject/service/ParkingManagerService.java
@@ -3,7 +3,7 @@ package br.com.fcamara.parkingproject.service;
 import br.com.fcamara.parkingproject.controller.dto.ParkingManagerDto;
 import br.com.fcamara.parkingproject.controller.form.NewVehicleForm;
 import br.com.fcamara.parkingproject.controller.form.VehicleForm;
-import br.com.fcamara.parkingproject.exception.ApiRequestException;
+import br.com.fcamara.parkingproject.exception.ApiException;
 import br.com.fcamara.parkingproject.model.*;
 import br.com.fcamara.parkingproject.repository.AddressRepository;
 import br.com.fcamara.parkingproject.repository.ParkingManagerRepository;
@@ -18,12 +18,15 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingManagerService {
+
+    private final HttpStatus created = HttpStatus.CREATED;
+    private final HttpStatus notFound = HttpStatus.NOT_FOUND;
+    private final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
     @Autowired
     private VehicleRepository vehicleRepository;
@@ -51,26 +54,32 @@ public class ParkingManagerService {
             return allParkedVehicle.stream().map(ParkingManagerDto::new).collect(Collectors.toList());
         }
 
-        throw new ApiRequestException("Endereço não encontrado.");
+        return null;
+
     }
 
-    public ResponseEntity<ParkingManagerDto> register(VehicleForm form) {
+    public ResponseEntity<Object> register(VehicleForm form) {
 
         Optional<Address> address = addressRepository.findByZip(form.getZip());
-        Optional<Vehicle> vehicle = vehicleRepository.findByLicensePlate(form.getLicensePlate());
+        Optional<Vehicle> vehicle = vehicleRepository.findByLicensePlate(form.getLicensePlate().toUpperCase());
 
         if (address.isPresent() && vehicle.isPresent()) {
             if (entranceValidate(address.get(), vehicle.get())) {
-                return ResponseEntity.status(HttpStatus.CREATED)
-                        .body(save(address.get(), vehicle.get()));
+
+                return ResponseEntity.status(created).body(save(address.get(), vehicle.get()));
+
+            }
+            else {
+
             }
         }
 
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(notFound)
+                .body(new ApiException("Endereço ou veículo invalido.", notFound));
 
     }
 
-    public ResponseEntity<?> registerNew(NewVehicleForm form) {
+    public ResponseEntity<Object> registerNew(NewVehicleForm form) {
         Optional<Address> address = addressRepository.findByZip(form.getZip());
         Boolean existsVehicle = vehicleRepository.existsByLicensePlate(form.getLicensePlate());
 
@@ -80,20 +89,23 @@ public class ParkingManagerService {
 
             if (hasSpace(address.get(), vehicle)){
 
-                return ResponseEntity.status(HttpStatus.CREATED)
+                return ResponseEntity.status(created)
                         .body(save(address.get(), vehicle));
             }else{
 
-                return ResponseEntity.status(HttpStatus.CREATED)
-                        .body("Veículo cadastrado, porém não foi possivel estaciona-lo. Pois o não há espaço");
+                return ResponseEntity.status(created)
+                        .body(new ApiException("Veículo cadastrado, porém não foi possivel " +
+                                "estaciona-lo. Pois não há espaço.", created));
 
             }
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.status(badRequest)
+                .body(new ApiException("O endereço não foi encontrado. " +
+                        "Ou o veículo já está cadastrado.", badRequest));
     }
 
-    public ResponseEntity<?> registerExit(Long id) {
+    public ResponseEntity<Object> registerExit(Long id) {
         Optional<Vehicle> vehicle = vehicleRepository.findById(id);
 
         if (vehicle.isPresent()) {
@@ -106,11 +118,17 @@ public class ParkingManagerService {
                 parkingVechile.get().setStatus(VehicleStatus.ESTACIONOU);
 
                 return ResponseEntity.status(HttpStatus.OK).build();
+
+            }else {
+                return ResponseEntity.status(notFound)
+                        .body(new ApiException("Veículo não está estacionado.", notFound));
             }
 
         }
 
-        return ResponseEntity.notFound().build();
+            return ResponseEntity.status(notFound)
+                    .body(new ApiException("Veículo não encontrado.", notFound));
+
 
     }
 
